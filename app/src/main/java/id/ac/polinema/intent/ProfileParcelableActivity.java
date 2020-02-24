c9ad04a279a9e72e3904daf3aa468eda415f82da
kiki <rahmarizki37@gmail.com>
2020-02-23 18:05:55 -0800

    Parcelable pengaturan class ParcelableActivity penambahan handleSubmit atribut onClick menambahkan setText (tampilan pada profile)

--- app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -1,10 +1,19 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import id.ac.polinema.intent.model.User;
+import static id.ac.polinema.intent.ParcelableActivity.USER_KEY;
+
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private TextView usernameText;
+    private TextView nametext;
+    private TextView ageText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +21,16 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
+        usernameText = findViewById(R.id.text_username);
+        nametext = findViewById(R.id.text_name);
+        ageText = findViewById(R.id.text_age);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-
+        User user = getIntent().getParcelableExtra(USER_KEY);
+        if (user != null) {
             // TODO: display value here
+            usernameText.setText(user.getUsername());
+            nametext.setText(user.getName());
+            ageText.setText(Integer.toString(user.getAge()));
         }
     }
 }
